bf1c28a4825f289f25f689b2af8dbbd49c6b8dc0
barrosorr <barroso.5055@gmail.com>
2024-08-01 16:26:06 -0400

    Add project files.

---
+++ Lab03-ASPNET/Controllers/HomeController1.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Lab03_ASPNET.Data;
+using Lab03_ASPNET.Models;
+
+
+namespace Lab03_ASPNET.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        private readonly DataAccess dataAccess;
+
+        public HomeController(DataAccess dataAccess)
+        {
+            this.dataAccess = dataAccess;
+        }
+
+        public IActionResult Index()
+        {
+            List<YourModel> data = dataAccess.GetData();
+            return View(data);
+
+        }
+    }
+}
